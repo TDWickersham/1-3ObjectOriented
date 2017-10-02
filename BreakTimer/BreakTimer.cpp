1c05526e392ff58149c40c2ec06b31e6e8ea37c9
s179067 <s179067@SEA-617629>
2017-10-02 10:09:04 -0700

    beep beep im a sheep

--- BreakTimer/BreakTimer.cpp
+++ BreakTimer/BreakTimer.cpp
@@ -2,25 +2,29 @@
 #include <iostream>
 #include "Timer.h"
 #include <string>
+
 int main()
 {
+	float timeSec;
+	std::cout << "how long do you want your timer?" << std::endl;
+
+	std::cin >> timeSec;
+
+
 	sfw::initContext(800, 600, "BreakTimer");
 
 	unsigned int imgFont = sfw::loadTextureMap("res/fontmap.png", 16, 16);
-	float timeSec = 0;
+	
 
 	timer beep;
 
+	sfw::setBackgroundColor(BLACK);
+
 	float DT;
 	beep.Reset(timeSec);
+	beep.StartTimer = timeSec;
 	while (sfw::stepContext())
 	{
-		std::cout << "how long do you want your timer?" << std::endl;
-
-		std::cin >> timeSec;
-
-
-		
 
 		DT = sfw::getDeltaTime();
 
